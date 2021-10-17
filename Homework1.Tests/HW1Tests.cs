b86299b8a5642bec3d13eab78095575d075f4f96
Aufniy Tiger 228 <golenkotoxa@gmail.com>
2021-10-18 00:36:53 +0300

    i dont understand GIT

--- Homework1.Tests/HW1Tests.cs
+++ Homework1.Tests/HW1Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace Homework1.Tests
 {
@@ -12,19 +13,32 @@ namespace Homework1.Tests
             _hw1 = new HW1();
         }
 
-        [Test]
-        public void FindASolutionUsingTheFormulaTest()
+        [TestCase(5, 9, 26.5)]
+        public void FindASolutionUsingTheFormulaTest(double numberA, double numberB, 
+            double expectedResult)
         {
             //arrange - готовим переменные
-            double numberA = 5;
-            double numberB = 9;
-            double expectedResult = 26.5;
 
             //act - вызываем метод 
             double actualResult = _hw1.FindASolutionUsingTheFormula(numberA, numberB);
 
             //assert - проверяем результаты
             Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestCase(5, 5,
+            "Ошибка. Деление на ноль... Числа не должны быть равны")]
+        public void FindASolutionUsingTheFormulaNegativeTest(
+            double numberA, double numberB, string expectedMessage
+            )
+        {
+            //arrange - готовим переменные
+
+            //act - вызываем метод 
+            Exception ex = Assert.Throws(typeof(DivideByZeroException), 
+                () => _hw1.FindASolutionUsingTheFormula(numberA, numberB));
+
+            //assert - проверяем результаты
+            Assert.AreEqual(expectedMessage, ex.Message);
         }
 
         [Test]
@@ -33,10 +47,21 @@ namespace Homework1.Tests
             Assert.Pass();
         }
 
-        [Test]
-        public void Test3()
+        [TestCase(2, 3, 0)]
+        [TestCase(-6, -3, 2)]
+        [TestCase(0, 3, 0)]
+        [TestCase(7, 3, 2)]
+        public void CalculateTheWholePartOfTheDivisionTest(double numberA, double numberB,
+           int expectedResult)
         {
-            Assert.Pass();
+            //arrange - готовим переменные
+
+            //act - вызываем метод 
+            int actualResult = _hw1.CalculateTheWholePartOfTheDivision(numberA, numberB);
+
+            //assert - проверяем результаты
+            Assert.AreEqual(expectedResult, actualResult);
+            
         }
     }
 }
