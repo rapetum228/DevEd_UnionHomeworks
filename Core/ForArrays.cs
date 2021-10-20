74b2afb9b12a1be114aa95f75f70e9ab8bbd86a8
Aufniy Tiger 228 <golenkotoxa@gmail.com>
2021-10-20 10:47:22 +0300

    Добавил библиотеку классов статических методов

---
+++ Core/ForArrays.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Core
+{
+    public class ForArrays
+    {
+        public void ShowMeAnArrayAnScreen(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]}\t");
+            Console.WriteLine("\n");
+        }
+    }
+}
