cfffd951eb34dfccb19e41eba765f5d99c2f9172
Aufniy Tiger 228 <golenkotoxa@gmail.com>
2021-10-21 18:40:48 +0300

    Создал юнит тесты к 4 ДЗ (вот бы их опять не профукать :D)

---
+++ Homework4.Tests/HW4Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Homework4.Tests
+{
+    public class HW4Tests
+    {
+        private HW4 _hw4;
+        [SetUp]
+        public void Setup()
+        {
+            _hw4 = new HW4();
+        }
+    }
+}
