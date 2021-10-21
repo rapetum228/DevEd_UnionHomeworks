78d9e2f865d5a17a0f94af1125244edeabab3b4b
Aufniy Tiger 228 <golenkotoxa@gmail.com>
2021-10-21 16:54:49 +0300

    переименовад на Helper

--- Core/Helper.cs
+++ Core/Helper.cs
@@ -2,13 +2,20 @@
 
 namespace Core
 {
-    public class ForArrays
+    public class Helper
     {
         public void ShowMeAnArrayAnScreen(int[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
                 Console.Write($"{arr[i]}\t");
             Console.WriteLine("\n");
+        }
+
+        public void SwapTheVariables(ref int a, ref int b)
+        {
+            int copyA = a;
+            a = b;
+            b = copyA;
         }
     }
 }
