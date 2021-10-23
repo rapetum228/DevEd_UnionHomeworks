d9ca7c0d770a390060e4030ebd0cab3dc4bf1f30
Aufniy Tiger 228 <golenkotoxa@gmail.com>
2021-10-23 11:03:01 +0300

    создал библиотеку классов ArrayList и некоторые методы

--- AllHomeworks/Program.cs
+++ AllHomeworks/Program.cs
@@ -1,4 +1,5 @@
-﻿using Homework1;
+﻿using ArrayListLibrary;
+using Homework1;
 using Homework2;
 using Homework3;
 using Homework4;
@@ -11,10 +12,28 @@ namespace AllHomeworks
     {
         static void Main(string[] args)
         {
-            HW3 hw = new HW3();
+            ArrayList aList = new ArrayList(new int[]{1, 2, 3 });
+            aList.AddFirst(45);
+            aList.AddLast(9);
+            aList.AddFirst(15);
+            aList.AddFirst(15);
+            aList.AddFirst(15);
+            aList.AddLast(15);
+            aList.AddLast(15);
+            aList.AddLast(15);
+            aList.AddLast(15);
+            aList.AddFirst(20);
+            aList.AddFirst(20);
+            aList.AddFirst(20);
+            aList.AddFirst(20);
+            aList.AddFirst(20);
+            aList.AddFirst(20);
+            aList.AddLast(20);
+            aList.AddLast(29);
+            aList.AddLast(23);
+            aList.AddLast(22);
 
-            hw.SolveTask11();
-
+            Console.ReadLine();
         }
     }
 }
