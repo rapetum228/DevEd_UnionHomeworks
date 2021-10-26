cad443cc8641acbbcda597b9e7121e07b3b89049
Aufniy Tiger 228 <golenkotoxa@gmail.com>
2021-10-26 11:56:40 +0300

    добавил несколько методов в linked list

--- AllHomeworks/Program.cs
+++ AllHomeworks/Program.cs
@@ -4,6 +4,7 @@ using Homework2;
 using Homework3;
 using Homework4;
 using Homework5;
+using LinkedLists;
 using System;
 
 namespace AllHomeworks
@@ -12,6 +13,7 @@ namespace AllHomeworks
     {
         static void Main(string[] args)
         {
+            /*
             ArrList aList = new ArrList();
             ArrList aList1 = new ArrList(new int[] { 1, 2, 3 });
             aList.AddFirst(aList1);
@@ -19,6 +21,23 @@ namespace AllHomeworks
             aList.RemoveAll(8);
             aList.AddLast(aList1);
             Console.ReadLine();
+            */
+
+            LinkList lList = new LinkList(1);
+            lList.AddLast(2);
+            lList.AddLast(3);
+            lList.AddLast(4);
+            lList.AddFirst(0);
+            lList.AddLast(5);
+            lList.AddLast(6);
+            lList.AddLast(7);
+            lList.AddAt(4, -4);
+            LinkList lList1 = new LinkList(-9);
+            lList1.AddLast(-10);
+            lList1.AddFirst(-8);
+            lList1.AddAt(2, -11);
+            lList.AddFirst(lList1);
+
         }
     }
 }
---
+++ LinkedLists/LinkList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    public class LinkList
+    {
+        public Node Head { get; private set; }
+        private Node _tail;
+
+        public LinkList(int value)
+        {
+            Head = new Node
+            {
+                Value = value
+            };
+            _tail = Head;
+        }
+
+        public void AddLast(int val)
+        {
+            Node current = new Node { Value = val};
+            _tail.Next = current;
+            _tail = current;
+        }
+        
+        public void AddLast(LinkList list)
+        {
+            _tail.Next = list.Head;
+        }
+       
+        public void AddFirst(int val)
+        {
+            Node current = new Node { Value = val };
+            current.Next = Head;
+            Head = current;
+        }
+
+        public void AddFirst(LinkList list)
+        {
+            Head = list.Head.Next;
+        }
+
+        public void AddAt(int idx, int val)
+        {
+            Node current = new Node { Value = val };
+            Node temp = Head;
+            for (int i = 0; i < idx-1; i++)
+            {
+                temp = temp.Next;
+            }
+
+            current.Next = temp.Next;
+            temp.Next = current;
+            //temp.Next = new Node { Value = 228 };
+            //temp.Next = new Node { Value = 229 };
+            temp.Next = current;
+        }
+
+
+    }
+}
