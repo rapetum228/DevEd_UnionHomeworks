af44bc4fcfead07f852c0123e31a40e46ff9b59d
Aufniy Tiger 228 <golenkotoxa@gmail.com>
2021-10-27 13:23:45 +0300

    вроде всё, но какой-то метод ломает lList1

--- LinkedLists/LinkList.cs
+++ LinkedLists/LinkList.cs
@@ -10,10 +10,10 @@ namespace LinkedLists
     {
         public Node Head { get; private set; }
         public Node Tail { get; private set; }
-        private int _lengthList;
+        public int LengthList { get; private set; }
         public LinkList(int value)
         {
-            _lengthList = 1;
+            LengthList = 1;
             Head = new Node
             {
                 Value = value
@@ -28,7 +28,7 @@ namespace LinkedLists
                 Value = list.Head.Value,
             };
             Node tempUS = Head;
-            _lengthList = 1; 
+            LengthList = 1; 
             
             Node temp = list.Head;
             while (temp.Next != null)
@@ -37,13 +37,14 @@ namespace LinkedLists
                 Node current = new Node { Value = temp.Value };
                 tempUS.Next = current;
                 tempUS = tempUS.Next;
-                _lengthList++;
+                LengthList++;
             }
             Tail = tempUS;//new Node { Value = list.Tail.Value, Next = null };
         }
 
         public void AddLast(int val)
         {
+            LengthList++;
             Node current = new Node { Value = val};
             Tail.Next = current;
             Tail = current;
@@ -51,6 +52,7 @@ namespace LinkedLists
         
         public void AddLast(LinkList list)
         {
+            LengthList += list.LengthList;
             LinkList temp = new LinkList(list);
             Tail.Next = list.Head;
             Tail = list.Tail;
@@ -59,6 +61,7 @@ namespace LinkedLists
        
         public void AddFirst(int val)
         {
+            LengthList++;
             Node current = new Node { Value = val };
             current.Next = Head;
             Head = current;
@@ -66,6 +69,7 @@ namespace LinkedLists
 
         public void AddFirst(LinkList list)
         {
+            LengthList += list.LengthList;
             LinkList temp = new LinkList(list);
             list.Tail.Next = Head;
             Head = list.Head;
@@ -74,6 +78,7 @@ namespace LinkedLists
 
         public void AddAt(int idx, int val)
         {
+            LengthList++;
             Node current = new Node { Value = val };
             Node temp = Head;
             for (int i = 0; i < idx-1; i++)
@@ -89,13 +94,13 @@ namespace LinkedLists
         public void AddAt(int idx, LinkList list)
         {
             LinkList newList = new LinkList(list);
+            LengthList += list.LengthList;
             Node temp = Head;
             for (int i = 0; i < idx - 1; i++)
                 temp = temp.Next;
 
             newList.Tail.Next = temp.Next;
             temp.Next = newList.Head;
-
         }
 
         public void Set(int idx, int val)
@@ -109,10 +114,12 @@ namespace LinkedLists
 
         public void RemoveFirst()
         {
+            LengthList--;
             Head = Head.Next;
         }
         public void RemoveLast()
         {
+            LengthList--;
             Node temp = Head;
             while (temp.Next.Next != null)
                 temp = temp.Next;
@@ -123,6 +130,7 @@ namespace LinkedLists
 
         public void RemoveAt(int idx)
         {
+            LengthList--;
             Node temp = Head;
             for (int i = 0; i < idx - 1; i++)
                 temp = temp.Next;
@@ -132,19 +140,252 @@ namespace LinkedLists
 
         public void RemoveFirstMultiple(int n)
         {
+            LengthList -= n;
             for (int i = 0; i < n; i++)
                 Head = Head.Next;
         }
 
         public void RemoveLastMultiple(int n)
         {
+            LengthList -= n;
             Node temp = Head;
-            while(temp.Next.Next.Next.Next != null)
+            for (int i = 0; i < LengthList-1; i++)
             {
-
+                temp = temp.Next;
             }
-                
+            Tail = temp;
+            temp.Next = null;
+
         }
 
+        public void RemoveAtMultiple(int idx, int n)
+        {
+            LengthList -= n;
+            Node temp = Head;
+            for (int i = 0; i < idx-1 ; i++)
+                temp = temp.Next;
+            
+            Node current = temp;
+
+            for (int i = 0; i < n+1; i++)
+                temp = temp.Next;
+            current.Next = temp;
+        }
+
+        public int RemoveFirst(int val)//поправить
+        {
+            if (Head.Value == val)
+            {
+                LengthList--;
+                Head = Head.Next;
+                return 0;
+            }
+                
+            Node temp = Head;
+            int index = 0;
+            while (temp.Next.Value != val)
+            {
+                index++;
+                if (temp.Next == null)
+                    return -1;
+                temp = temp.Next;
+            }  
+            temp.Next = temp.Next.Next;
+            LengthList--;
+            return index;
+        }
+
+        public int RemoveAll(int val)
+        {
+            int numbersOfRemoveElements = 0;
+            while (Head.Value == val)
+            {
+                numbersOfRemoveElements++;
+                Head = Head.Next;
+            }
+
+            Node temp = Head;
+
+            while (temp.Next != null)
+            {
+                if (temp.Next.Value == val)
+                {
+                    numbersOfRemoveElements++;
+                    temp.Next = temp.Next.Next;
+                }
+                else { temp = temp.Next; }
+            }
+            
+            LengthList -= numbersOfRemoveElements;
+            return numbersOfRemoveElements;
+        }
+
+        public bool Contains(int val)
+        {
+            if (Head.Value == val)
+                return true;
+            Node temp = Head;
+
+            while (temp.Value != val)
+            {
+                if (temp.Next == null)
+                    return false;
+                temp = temp.Next;
+            }
+
+            return true;
+        }
+
+        public int IndexOf(int val)
+        {
+            if (Head.Value == val)
+                return 0;
+            Node temp = Head;
+            int index = 0;
+            while (temp.Value != val)
+            {
+                if (temp.Next == null)
+                    return -1;
+                temp = temp.Next;
+                index++;
+            }
+
+            return index;
+        }
+
+        public int GetFirst()
+        {
+            return Head.Value;
+        }
+
+        public int GetLast()
+        {
+            return Tail.Value;
+        }
+
+        public int Get(int idx)
+        {
+            Node temp = Head;
+            for (int i = 0; i < idx; i++)
+                temp = temp.Next;
+            return temp.Value;
+        }
+
+        public void Reverse()//кажись всё..
+        {
+            int fakeLength = LengthList;
+            Node tempTail = Tail;
+            while (fakeLength > 1)
+            {
+                Node temp = Head;
+                for (int i = 0; i < fakeLength-2; i++)
+                {
+                    temp = temp.Next;
+                }
+                fakeLength--;
+                tempTail.Next = new Node {Value = temp.Value };
+                tempTail = tempTail.Next;
+            }
+            Head = Tail;
+            Tail = new Node { Value = tempTail.Value };
+        }
+
+        public int Max()
+        {
+            Node temp = Head;
+            int max = Head.Value;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+                if (max < temp.Value )
+                {
+                    max = temp.Value;
+                }
+            }
+            return max;
+        }
+
+        public int Min()
+        {
+            Node temp = Head;
+            int min = Head.Value;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+                if (min > temp.Value)
+                {
+                    min = temp.Value;
+                }
+            }
+            return min;
+        }
+
+        public int IndexOfMax()
+        {
+            Node temp = Head;
+            int max = Head.Value;
+            int indexOfMax = 0;
+            int index = 0;
+            while (temp.Next != null)
+            {
+                index++;
+                temp = temp.Next;
+                if (max < temp.Value)
+                {
+                    indexOfMax = index;
+                    max = temp.Value;
+                }
+            }
+            return indexOfMax;
+        }
+
+        public int IndexOfMin()
+        {
+            Node temp = Head;
+            int min = Head.Value;
+            int indexOfMin = 0;
+            int index = 0;
+            while (temp.Next != null)
+            {
+                index++;
+                temp = temp.Next;
+                if (min > temp.Value)
+                {
+                    indexOfMin = index;
+                    min = temp.Value;
+                }
+            }
+            return indexOfMin;
+        }
+
+        public void Sort()
+        {
+            int min = this.Min();
+            LinkList sortList = new LinkList(min);
+            LinkList copyThis = new LinkList(this);
+            while(copyThis.LengthList > 1)
+            {
+                copyThis.RemoveFirst(min);
+                min = copyThis.Min();
+                sortList.AddLast(min);
+            }
+            Head = sortList.Head;
+            Tail = sortList.Tail;
+        }
+
+        public void SortDesc()
+        {
+            int max = this.Max();
+            LinkList sortList = new LinkList(max);
+            LinkList copyThis = new LinkList(this);
+            while (copyThis.LengthList > 1)
+            {
+                copyThis.RemoveFirst(max);
+                max = copyThis.Max();
+                sortList.AddLast(max);
+            }
+            Head = sortList.Head;
+            Tail = sortList.Tail;
+        }
     }
 }
